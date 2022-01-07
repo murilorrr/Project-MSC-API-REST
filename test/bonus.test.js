0cc3ca21db56fdf83aa9214021270bbcfdffed7e
Murilo Ribeiro <murilorsv14@gmail.com>
2022-01-07 15:00:03 -0300

    delete mongourl on bonus.test.js

--- test/bonus.test.js
+++ test/bonus.test.js
@@ -5,8 +5,6 @@ const path = require('path');
 
 const exec = util.promisify(callbackExec);
 
-const mongoDbUrl = 'mongodb://localhost:27017';
-const url = 'http://localhost:3000';
 const NPX_NYC_COMMAND =
   (unit) => `npx nyc --all --include ${unit} --reporter json-summary mocha test/unit/${unit}.js --exit`;
 
